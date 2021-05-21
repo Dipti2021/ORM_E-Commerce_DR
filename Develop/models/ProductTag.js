c8d77c97fc08a2a51ecadf06cf92df37ce1c0aa5
Dipti R <razdan.dipti@gmail.com>
2021-05-21 17:33:20 -0400

    Updated product tags

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -18,15 +18,15 @@ ProductTag.init(
       references: {
         model: 'product',
         key: 'id'
-      }
+      },
     },
     tag_id: {
       type: DataTypes.INTEGER,
       references: {
         model: 'tag',
         key: 'id'
-      }
-    }
+      },
+    },
    
   },
   {
